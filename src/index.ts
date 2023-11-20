9f52a4b72a4460aeaaa16436c643d26b0d7a8517
Jorge <jorge.delgado@metrica.es>
2023-11-20 19:43:56 +0100

    storybook

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as Button } from './componets/atoms/Button/Button';
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'tech-mech-lib',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+                '@babel/preset-env',
+                '@babel/preset-typescript'
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
